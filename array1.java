c4b66c03a86b065b59d31b936919433f7a4dc15a
Dawood ahmad <143737131+dawood078@users.noreply.github.com>
2025-12-30 23:39:28 +0530

    Add files via upload

---
+++ array1.java
@@ -0,0 +1,94 @@
+// import java.util.Scanner;
+
+// class ArrayDemo {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+
+//         int[] arr = new int[5];
+
+//         System.out.println("Enter 5 elements:");
+//         for (int i = 0; i < 5; i++) {
+//             arr[i] = sc.nextInt();
+//         }
+
+//         System.out.println("Array elements:");
+//         for (int i = 0; i < 5; i++) {
+//             System.out.print(arr[i] + " ");
+//         }
+//     }
+// }
+
+
+// class ArraySum {
+//     public static void main(String[] args) {
+//         int[] arr = {10, 20, 30, 40, 50};
+//         int sum = 0;
+
+//         for (int i = 0; i < arr.length; i++) {
+//             sum += arr[i];
+//         }
+
+//         System.out.println("Sum = " + sum);
+//     }
+// }
+
+// class LargestElement {
+//     public static void main(String[] args) {
+//         int[] arr = {15, 25, 5, 45, 30};
+//         int max = arr[0];
+
+//         for (int i = 1; i < arr.length; i++) {
+//             if (arr[i] > max) {
+//                 max = arr[i];
+//             }
+//         }
+
+//         System.out.println("Largest = " + max);
+//     }
+// }
+
+// class ReverseArray {
+//     public static void main(String[] args) {
+//         int[] arr = {1, 2, 3, 4, 5};
+
+//         System.out.println("Reversed array:");
+//         for (int i = arr.length - 1; i >= 0; i--) {
+//             System.out.print(arr[i] + " ");
+//         }
+//     }
+// }
+
+// import java.util.Scanner;
+
+// class Matrix {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+
+//         int[][] mat = new int[2][2];
+
+//         System.out.println("Enter matrix elements:");
+//         for (int i = 0; i < 2; i++) {
+//             for (int j = 0; j < 2; j++) {
+//                 mat[i][j] = sc.nextInt();
+//             }
+//         }
+
+//         System.out.println("Matrix:");
+//         for (int i = 0; i < 2; i++) {
+//             for (int j = 0; j < 2; j++) {
+//                 System.out.print(mat[i][j] + " ");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+// Sorting array (Bubble / Selection)
+
+// Searching (Linear / Binary)
+
+// Remove duplicates
+
+// Even-odd count
+
+// Interview & viva array questions
